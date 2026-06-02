11448a558526c05bcd18827f4da4a9c7c5d66052
Navya Srivastava <143245639+NSrivastava1012@users.noreply.github.com>
2026-06-02 10:50:46 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import joblib
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+
+# Load dataset
+df = pd.read_csv(
+    "dataset/train.tsv",
+    sep="\t",
+    header=None
+)
+
+# Add column names
+df.columns = [
+    "text",
+    "emotion",
+    "id"
+]
+
+# Features and labels
+X = df["text"]
+y = df["emotion"]
+
+# Text vectorizer
+vectorizer = TfidfVectorizer(
+    max_features=5000
+)
+
+X_vectorized = vectorizer.fit_transform(X)
+
+# Train model
+model = LogisticRegression(
+    max_iter=1000
+)
+
+model.fit(
+    X_vectorized,
+    y
+)
+
+# Save trained model
+joblib.dump(
+    model,
+    "model/mood_model.pkl"
+)
+
+# Save vectorizer
+joblib.dump(
+    vectorizer,
+    "model/vectorizer.pkl"
+)
+
+print("Model + Vectorizer saved successfully!")
