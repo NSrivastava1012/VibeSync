a347f7ab6a61e8d5a5362810f4a3ce3f848dd97a
Navya Srivastava <143245639+NSrivastava1012@users.noreply.github.com>
2026-06-03 01:02:55 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from functools import wraps
 from flask import session, redirect, url_for
 
@@ -14,23 +13,4 @@ def login_required(f):
             )
 
         return f(*args, **kwargs)
-
-=======
-from functools import wraps
-from flask import session, redirect, url_for
-
-def login_required(f):
-
-    @wraps(f)
-    def decorated(*args, **kwargs):
-
-        if "user_id" not in session:
-
-            return redirect(
-                url_for("login")
-            )
-
-        return f(*args, **kwargs)
-
->>>>>>> 236cf661cc88def0fa39c2dc0c47a7d964d8c411
     return decorated
