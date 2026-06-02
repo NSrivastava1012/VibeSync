679b0df0671c056c353e6d9a59ce0bac936aad2e
Navya Srivastava <143245639+NSrivastava1012@users.noreply.github.com>
2026-06-03 01:08:58 +0530

    Update test_recommend.py

--- test_recommend.py
+++ test_recommend.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-from model.recommend import recommend_playlist
+from mood_model.recommend import recommend_playlist
 
 while True:
 
@@ -13,20 +12,4 @@ while True:
     print("Recommended Playlists:")
 
     for playlist in result["playlists"]:
-=======
-from model.recommend import recommend_playlist
-
-while True:
-
-    text = input("Enter mood: ")
-
-    result = recommend_playlist(text)
-
-    print("\nDetected Emotion:",
-          result["emotion"])
-
-    print("Recommended Playlists:")
-
-    for playlist in result["playlists"]:
->>>>>>> 236cf661cc88def0fa39c2dc0c47a7d964d8c411
         print("-", playlist)
