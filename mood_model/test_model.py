5031220d3854277ee2a2d0948581c278dd4d6310
Navya Srivastava <143245639+NSrivastava1012@users.noreply.github.com>
2026-06-02 21:52:27 +0530

    Update test_model.py

--- mood_model/test_model.py
+++ mood_model/test_model.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import joblib
 
 
@@ -32,41 +31,4 @@ while True:
 
     print(
         "\nPredicted Emotion:",
-        prediction
-=======
-import joblib
-
-
-# ==========================
-# LOAD MODEL
-# ==========================
-
-model = joblib.load(
-    "model/mood_model.pkl"
-)
-
-print("Model Loaded Successfully!")
-
-
-# ==========================
-# TEST SENTENCES
-# ==========================
-
-while True:
-
-    text = input(
-        "\nEnter your mood (or type exit): "
-    )
-
-    if text.lower() == "exit":
-        break
-
-    prediction = model.predict(
-        [text]
-    )[0]
-
-    print(
-        "\nPredicted Emotion:",
-        prediction
->>>>>>> 236cf661cc88def0fa39c2dc0c47a7d964d8c411
-    )
+        prediction)
